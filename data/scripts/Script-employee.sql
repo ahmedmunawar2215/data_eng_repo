af1ca5e8b420be0012a436a64325e751e27e1c62
Derya Oruç <deryaoruc00@gmail.com>
2022-09-21 01:46:03 +0300

    fixed minor typos in the table

--- data/scripts/Script-employee.sql
+++ data/scripts/Script-employee.sql
@@ -9,7 +9,7 @@ TitleOfCourtesy ||' '||LastName AS employee_name,
 ROUND((JULIANDAY('now') - JULIANDAY(BirthDate))/360.25)  AS employee_age, 
 ROUND((JULIANDAY('now') - JULIANDAY(HireDate))/360.25) AS working_year
 FROM Employees e 
-LEFT JOIN mytemp USING(EmployeeID);
+LEFT JOIN employees_age_temp USING(EmployeeID);
 
 
 -- Age and Seniority Calculations of Employees by City
@@ -18,8 +18,8 @@ CREATE TEMP TABLE employees_calculation_temp AS
 SELECT  employee_city, COUNT(EmployeeID),
 MAX(employee_age), MIN(employee_age), ROUND(AVG(employee_age)), 
 MAX(working_year), MIN(working_year), ROUND(AVG(working_year))
-FROM mytemp
-GROUP BY employee_city
+FROM employees_age_temp
+GROUP BY employee_city;
 
 
 -- Employees Seniorty and Retaire Status
@@ -35,7 +35,7 @@ CASE
 	WHEN employee_age > 65 THEN "Can Retaire"
 	ELSE "Can't Retaire"
 END Retaire_Status
-FROM mytemp
+FROM employees_age_temp
 GROUP BY EmployeeID;
 
 --
