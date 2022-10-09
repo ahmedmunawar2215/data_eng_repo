f54607b5dac0e631ea29d7b9df191c824bbc8d6a
ahmedmunawar2215 <ahmedmunawar360180@gmail.com>
2022-10-10 01:59:30 +0300

    Commited cahnges on behald of furkan

--- data/scripts/MAIN.sql
+++ data/scripts/MAIN.sql
@@ -267,3 +267,221 @@ FROM Customers c
 Group by Country, City
 
 SELECT * FROM customer_by_city
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+
+
+-- First_Script
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- Employees by Title
+create temp table employees_by_title as
+SELECT
+	Title,
+	COUNT(EmployeeID) as number_of_employee
+  FROM
+  Employees
+  GROUP BY Title
+  ORDER BY Title 
+
+-------------------------------------------------------------------------------- 
+
+ -- Employee Distribution by City
+ create temp table number_of_employee_residing as
+SELECT
+       MAX(City) AS cities,
+       COUNT(City) AS number_of_employee_residing 
+FROM 
+	Employees e 
+GROUP BY 
+	city 
+ORDER BY 
+	city 
+	
+--------------------------------------------------------------------------------
+	
+-- Employee Distribution by Country
+ create temp table number_of_employee_by_country as
+SELECT   
+       MAX(Country) AS countries,
+       COUNT(Country) AS number_of_employee_residing
+FROM 
+	Employees e 
+GROUP BY 
+	country
+ORDER BY 
+	country
+	
+--------------------------------------------------------------------------------
+
+-- Contact Info of Employees
+create temp table contact_info_temp as
+SELECT 
+	FirstName || ' ' || LastName ||' ' ||'can be reached at' ||' ' ||Extension AS Contactinfo
+FROM 
+	employees;
+
+--------------------------------------------------------------------------------
+
+-- Employees Sorted By Their Seniority
+create temp table employee_seniority_temp as
+SELECT 
+	FirstName || ' ' || LastName AS FullName, 
+	HireDate
+FROM 
+	Employees e 
+ORDER BY 
+	HireDate;
+
+--------------------------------------------------------------------------------
+
+-- Duration of employment
+create temp table employment_duration_temp as
+SELECT  
+	hiredate,
+	current_date,  
+	(current_date - HireDate) AS duration_of_employment
+from 
+	Employees ;
+
+--------------------------------------------------------------------------------
+
+-- List of The Roles in The Company
+create temp table list_of_roles_temp as
+SELECT 
+DISTINCT Title, 
+COUNT(Title) 
+FROM 
+	Employees
+GROUP BY 
+	Title
+	
+--------------------------------------------------------------------------------
+	
+	
+SELECT * FROM employee_statistics_temp;
+SELECT * FROM contact_info_temp;
+SELECT * FROM employee_seniority_temp;
+SELECT * FROM employment_duration_temp;
+SELECT * FROM list_of_roles_temp;
+
+
+DROP TABLE employee_statistics_temp;
+DROP TABLE contact_info_temp;
+DROP TABLE employee_seniority_temp;
+DROP TABLE employment_duration_temp;
+DROP TABLE list_of_roles_temp;
+
+
+
+
+
+
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+
+
+-- Second_Script
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- List of Employee Territories
+create temp table list_of_employee_territories_temp as
+SELECT * 
+FROM 
+	Territories t 
+LEFT JOIN 
+	EmployeeTerritories e 
+ON 
+	t.TerritoryID = e.TerritoryID
+
+--------------------------------------------------------------------------------
+
+-- Employees By Territories
+create temp table employees_by_territory_temp as
+SELECT  
+	FirstName || ' ' || LastName as Name,
+	et.TerritoryID ,
+	t.TerritoryDescription  
+FROM 
+	EmployeeTerritories et 
+LEFT JOIN 
+	Employees e 
+ON 
+	et.EmployeeID = e.EmployeeID
+LEFT JOIN
+	Territories t
+ON 
+	t.TerritoryID =et.TerritoryID 
+	
+--------------------------------------------------------------------------------
+	
+-- Total Territory By Employee
+create temp table total_territory_by_employee_temp as
+SELECT 
+	FirstName || ' ' || LastName as Name,
+	COUNT(et.TerritoryID) as Total_Territory
+FROM 
+	EmployeeTerritories et 
+LEFT JOIN 
+	Employees e 
+ON 
+	et.EmployeeID = e.EmployeeID
+GROUP BY 
+	Name
+ORDER BY	
+	Total_Territory DESC
+
+--------------------------------------------------------------------------------
+
+-- Who Reports To Who? (First)
+create temp table who_reports_who_temp as
+SELECT 
+	empa.employeeid, 
+	empa.firstname, 
+	empa.lastname, 
+	empb.firstname AS 'Reports to Manager' 
+FROM   
+	employees empa 
+INNER JOIN 
+	employees empb 
+ON 
+	empa.reportsto = empb.employeeid  
+	
+-------------------------------------------------------------------------------
+
+-- Who Reports To Who? (Second) 
+create temp table who_reports_who_second_temp as
+SELECT 
+	a.EmployeeID,
+	a.LastName || " " || a.FirstName as employee,
+	b.LastName||" " || b.FirstName  as manager
+FROM 
+	employees a
+LEFT JOIN 
+	employees b
+ON 
+	b.EmployeeID = a.ReportsTo
+ORDER BY 
+	a.EmployeeID;
+      
+-------------------------------------------------------------------------------
+
+
+SELECT * FROM list_of_employee_territories_temp;
+SELECT * FROM employees_by_territory_temp;
+SELECT * FROM total_territory_by_employee_temp;
+SELECT * FROM who_reports_who_temp;
+SELECT * FROM who_reports_who_second_temp;
+
+
+DROP TABLE list_of_employee_territories_temp;
+DROP TABLE employees_by_territory_temp;
+DROP TABLE total_territory_by_employee_temp;
+DROP TABLE who_reports_who_temp;
+DROP TABLE who_reports_who_second_temp;
